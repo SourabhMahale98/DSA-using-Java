588c861dd5291101b3d726269179a623a6bc097c
Sourabh Mahale <SourabhMahale98@gmail.com>
2024-09-09 11:07:00 +0530

    Added BinarySearch By Recursion

--- Searching/src/App.java
+++ Searching/src/App.java
@@ -4,15 +4,31 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         int sortedArray[] = { 1, 5, 6, 7, 9, 11, 13, 15 };
-        int target = 115;
+        int target = 13;
 
         int result1 = linerSearch(sortedArray, target);
         int result2 = binarySearch(sortedArray, target);
+        int result3 = binarySearchByRecursion(sortedArray, target, 0, sortedArray.length - 1);
 
-        if (result1 != -1)
-            System.out.println("Element found at index : " + result1);
+        if (result3 != -1)
+            System.out.println("Element found at index : " + result3);
         else
             System.out.println("Element not found");
+    }
+
+    public static int binarySearchByRecursion(int[] sortedArray, int target, int left, int right) {
+
+        if (left <= right) {
+            int mid = (left + right) / 2;
+            if (sortedArray[mid] == target) {
+                return mid;
+            } else if (sortedArray[mid] < target) {
+                return binarySearchByRecursion(sortedArray, target, mid + 1, right);
+            } else {
+                return binarySearchByRecursion(sortedArray, target, left, mid - 1);
+            }
+        }
+        return -1;
     }
 
     public static int linerSearch(int[] sortedArray, int target) {
