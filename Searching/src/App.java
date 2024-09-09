cddb9632be26b45c6f82f147aee89b852e0383fb
Sourabh Mahale <SourabhMahale98@gmail.com>
2024-09-09 10:47:26 +0530

    Linear and Binary Search

--- Searching/src/App.java
+++ Searching/src/App.java
@@ -1,5 +1,54 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int sortedArray[] = { 1, 5, 6, 7, 9, 11, 13, 15 };
+        int target = 115;
+
+        int result1 = linerSearch(sortedArray, target);
+        int result2 = binarySearch(sortedArray, target);
+
+        if (result1 != -1)
+            System.out.println("Element found at index : " + result1);
+        else
+            System.out.println("Element not found");
     }
+
+    public static int linerSearch(int[] sortedArray, int target) {
+        int steps = 0;
+
+        for (int i = 0; i < sortedArray.length; i++) {
+            steps++;
+            if (sortedArray[i] == target) {
+                System.out.println("Steps need in linearSearch is :" + steps);
+                return i;
+            }
+        }
+        System.out.println("Steps need in linearSearch is :" + steps);
+        return -1;
+    }
+
+    public static int binarySearch(int[] sortedArray, int target) {
+        int steps = 0;
+        int left = 0;
+        int right = sortedArray.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            steps++;
+            if (sortedArray[mid] == target) {
+                System.out.println("Steps need in BinerySearch is :" + steps);
+                return mid;
+            } else if (target > sortedArray[mid]) {
+                left = mid + 1;
+
+            } else {
+                right = mid - 1;
+            }
+        }
+        System.out.println("Steps need in BinerySearch is :" + steps);
+        return -1;
+    }
+
 }
